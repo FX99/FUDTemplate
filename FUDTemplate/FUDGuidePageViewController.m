8d179dc5b3b070a904a56ee805912c9aaccc00b7
兰福东 <fudonglan@sohu-inc.com>
2018-04-08 17:49:49 +0800

    添加引导页结尾的“立即体验”按钮

--- FUDTemplate/FUDGuidePageViewController.m
+++ FUDTemplate/FUDGuidePageViewController.m
@@ -17,13 +17,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    CGFloat height = self.view.frame.size.height;
+    CGFloat width  = self.view.frame.size.width;
+    
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, width, height)];
     scrollView.pagingEnabled = YES;
     scrollView.bounces = NO;
     [self.view addSubview:scrollView];
     
     for (int i=0; i<4; i++) {
-        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(i*self.view.frame.size.width, 0, self.view.frame.size.width, self.view.frame.size.height)];
+        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(i*width, 0, width, height)];
         if (i == 0) {
             view.backgroundColor = [UIColor orangeColor];
         } else if (i == 1) {
@@ -32,11 +35,21 @@
             view.backgroundColor = [UIColor blueColor];
         } else if (i == 3) {
             view.backgroundColor = [UIColor lightGrayColor];
+            UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+            button.frame = CGRectMake(100, height-100, width-200, 40);
+            button.backgroundColor = [UIColor orangeColor];
+            [button setTitle:@"立即体验" forState:UIControlStateNormal];
+            [button addTarget:self action:@selector(buttonHandler) forControlEvents:UIControlEventTouchUpInside];
+            [view addSubview:button];
         }
         
         [scrollView addSubview:view];
     }
     scrollView.contentSize = CGSizeMake(4*self.view.frame.size.width, self.view.frame.size.height);
+}
+
+- (void)buttonHandler {
+    
 }
 
 @end
