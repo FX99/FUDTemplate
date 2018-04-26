78cfb0170b4a036e50f26731bb3eaabea1e75ea3
兰福东 <fudonglan@sohu-inc.com>
2018-04-26 14:14:21 +0800

    更新目录结构。

---
+++ FUDTemplate/BaseUI/FUDTabBarController.m
@@ -0,0 +1,23 @@
+//
+//  FUDTabBarController.m
+//  FUDTemplate
+//
+//  Created by 兰福东 on 2018/4/26.
+//  Copyright © 2018年 兰福东. All rights reserved.
+//
+
+#import "FUDTabBarController.h"
+
+@interface FUDTabBarController ()
+
+@end
+
+@implementation FUDTabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+@end
